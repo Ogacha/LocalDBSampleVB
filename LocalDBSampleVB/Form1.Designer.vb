525aa1c499d3c05accfb5326b1cfcd174e7aeaec
Ogawa, Takasi <ogacha@gmail.com>
2016-12-01 17:43:56 +0900

    サンプル完成

--- LocalDBSampleVB/Form1.Designer.vb
+++ LocalDBSampleVB/Form1.Designer.vb
@@ -22,9 +22,116 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.cmdExecButton = New System.Windows.Forms.Button()
+        Me.tbConnectionString = New System.Windows.Forms.TextBox()
+        Me.lblConnectionString = New System.Windows.Forms.Label()
+        Me.tbCommand = New System.Windows.Forms.TextBox()
+        Me.lblCommand = New System.Windows.Forms.Label()
+        Me.dgvResultTable = New System.Windows.Forms.DataGridView()
+        Me.cmdCreateMdf = New System.Windows.Forms.Button()
+        CType(Me.dgvResultTable, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'cmdExecButton
+        '
+        Me.cmdExecButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmdExecButton.Location = New System.Drawing.Point(268, 229)
+        Me.cmdExecButton.Name = "cmdExecButton"
+        Me.cmdExecButton.Size = New System.Drawing.Size(75, 23)
+        Me.cmdExecButton.TabIndex = 0
+        Me.cmdExecButton.Text = "実行"
+        Me.cmdExecButton.UseVisualStyleBackColor = True
+        '
+        'tbConnectionString
+        '
+        Me.tbConnectionString.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tbConnectionString.Location = New System.Drawing.Point(12, 28)
+        Me.tbConnectionString.Multiline = True
+        Me.tbConnectionString.Name = "tbConnectionString"
+        Me.tbConnectionString.Size = New System.Drawing.Size(330, 51)
+        Me.tbConnectionString.TabIndex = 1
+        Me.tbConnectionString.Text = "Data Source=(LocalDB)\MSSQLLocalDB;" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "AttachDbFileName=C:\TEMP\LocalDBSample.mdf;" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Integrated Security=True;"
+        '
+        'lblConnectionString
+        '
+        Me.lblConnectionString.AutoSize = True
+        Me.lblConnectionString.Location = New System.Drawing.Point(13, 13)
+        Me.lblConnectionString.Name = "lblConnectionString"
+        Me.lblConnectionString.Size = New System.Drawing.Size(29, 12)
+        Me.lblConnectionString.TabIndex = 2
+        Me.lblConnectionString.Text = "接続"
+        '
+        'tbCommand
+        '
+        Me.tbCommand.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tbCommand.Location = New System.Drawing.Point(12, 110)
+        Me.tbCommand.Multiline = True
+        Me.tbCommand.Name = "tbCommand"
+        Me.tbCommand.Size = New System.Drawing.Size(330, 113)
+        Me.tbCommand.TabIndex = 1
+        Me.tbCommand.Text = resources.GetString("tbCommand.Text")
+        '
+        'lblCommand
+        '
+        Me.lblCommand.AutoSize = True
+        Me.lblCommand.Location = New System.Drawing.Point(13, 95)
+        Me.lblCommand.Name = "lblCommand"
+        Me.lblCommand.Size = New System.Drawing.Size(26, 12)
+        Me.lblCommand.TabIndex = 2
+        Me.lblCommand.Text = "SQL"
+        '
+        'dgvResultTable
+        '
+        Me.dgvResultTable.AllowUserToAddRows = False
+        Me.dgvResultTable.AllowUserToDeleteRows = False
+        Me.dgvResultTable.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvResultTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvResultTable.Location = New System.Drawing.Point(12, 258)
+        Me.dgvResultTable.Name = "dgvResultTable"
+        Me.dgvResultTable.ReadOnly = True
+        Me.dgvResultTable.RowTemplate.Height = 21
+        Me.dgvResultTable.Size = New System.Drawing.Size(331, 94)
+        Me.dgvResultTable.TabIndex = 3
+        '
+        'cmdCreateMdf
+        '
+        Me.cmdCreateMdf.Location = New System.Drawing.Point(12, 230)
+        Me.cmdCreateMdf.Name = "cmdCreateMdf"
+        Me.cmdCreateMdf.Size = New System.Drawing.Size(75, 23)
+        Me.cmdCreateMdf.TabIndex = 4
+        Me.cmdCreateMdf.Text = ".mdf 作成"
+        Me.cmdCreateMdf.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(355, 364)
+        Me.Controls.Add(Me.cmdCreateMdf)
+        Me.Controls.Add(Me.dgvResultTable)
+        Me.Controls.Add(Me.lblCommand)
+        Me.Controls.Add(Me.lblConnectionString)
+        Me.Controls.Add(Me.tbCommand)
+        Me.Controls.Add(Me.tbConnectionString)
+        Me.Controls.Add(Me.cmdExecButton)
+        Me.Name = "Form1"
+        Me.Text = "LocalDBSample"
+        CType(Me.dgvResultTable, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents cmdExecButton As Button
+    Friend WithEvents tbConnectionString As TextBox
+    Friend WithEvents lblConnectionString As Label
+    Friend WithEvents tbCommand As TextBox
+    Friend WithEvents lblCommand As Label
+    Friend WithEvents dgvResultTable As DataGridView
+    Friend WithEvents cmdCreateMdf As Button
 End Class
--- LocalDBSampleVB/Form1.vb
+++ LocalDBSampleVB/Form1.vb
@@ -1,3 +1,44 @@
-﻿Public Class Form1
+﻿Imports System.Data.SqlClient
+Public Class Form1
 
+    Private table As DataTable = New DataTable()
+
+    Private Sub cmdExecButton_Click(sender As Object, e As EventArgs) Handles cmdExecButton.Click
+        Try
+            ' 通常の ADO.NET と変わりません。トランザクション、ストアドプロシージャー、
+            ' ストアド関数など、ほぼフル機能が使えます。
+            ' DataSource に指定するのは、(LocalDB)\インスタンスです。
+            ' 2016 から規定のインスタンス名は、MSSQLLocalDB です。
+            ' AttachDbFileName= で接続する .mdf ファイルを指定できます。
+            ' ファイル指定を省略した場合は、既定のファイルにつなぎにいきます。
+            ' C:\Users\ユーザー\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\ 配下
+            ' システム データベースの照合順序は変更できず、SQL_Latin1_General_CP1_CI_AS です。
+            Using con As SqlConnection = New SqlConnection(tbConnectionString.Text)
+                con.Open()
+                Dim tran As SqlTransaction = con.BeginTransaction()
+                Dim cmd = New SqlCommand(tbCommand.Text, con, tran)
+                Dim adp = New SqlDataAdapter(cmd)
+                adp.Fill(table)
+                tran.Commit()
+            End Using
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+        End Try
+
+        dgvResultTable.DataSource = table
+    End Sub
+
+    ' .mdf ファイルを作成するときは、CREATE DATABASE を ON (NAME=, FILENAME=) を指定して
+    ' 作成します。この場合の接続は、既定の接続でよいでしょう。
+    Private Sub cmdCreateMdf_Click(sender As Object, e As EventArgs) Handles cmdCreateMdf.Click
+        Try
+            Using con As SqlConnection = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;Integrated Security=True;")
+                con.Open()
+                Dim cmd = New SqlCommand("CREATE DATABASE LocalDBSample ON (NAME='LocalDBSample', FILENAME='C:\TEMP\LocalDBSample.mdf')", con)
+                cmd.ExecuteNonQuery()
+            End Using
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+        End Try
+    End Sub
 End Class
